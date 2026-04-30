73a469b018eaee9bfb93791949eb98a48c8922f4
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2026-05-01 01:07:59 +0530

    feat: Update Firebase configuration to use environment variable for credentials

--- app/config/firebaseConfig.py
+++ app/config/firebaseConfig.py
@@ -1,8 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+import os
+import json
+load_dotenv()
+
+
+
+firebase_credentials_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
 
 # Firebase will automatically handle the PEM formatting inside the JSON file
-cred = credentials.Certificate('C:\\Users\\KIIT0001\\projects\\test-yt-ai\\backend\\firebase-config.json')
+cred = credentials.Certificate(json.loads(firebase_credentials_json))
 firebase_admin.initialize_app(cred)
 
 firebase_db = firestore.client()
