0590ab27b8274eb4148be042eff348d14827e648
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2026-05-01 14:13:21 +0530

    channged chat model

--- app/config/aiConfig.py
+++ app/config/aiConfig.py
@@ -17,5 +17,5 @@ embed_model = GoogleGenerativeAIEmbeddings(
 
 
 chat_model = ChatGoogleGenerativeAI(
-    model="models/gemini-3.1-flash-lite", google_api_key=api_key
+    model="models/gemini-3.1-flash-lite-preview", google_api_key=api_key
 )
